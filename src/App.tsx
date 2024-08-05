2a23eb487c66da78068e87108f54f14050a004bb
parmila366 <tparmila366@gmail.com>
2024-08-05 16:09:48 +0545

    add new line

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <>
    <h1 className='bg-red-800 text-white text-center'>hello</h1>
    <Home name={"parmila"} address={" devinagar"}/>
+   <h2>Hello i am parmila</h2>
     </>
   )
 }
